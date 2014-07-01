9bb7e45f9ec1215cf590e948e426cc567762026b
Alex Chaplinsky <alchapone@yandex.ru>
2014-07-01 21:13:36 +0300

    Simplified just to assets_tag_helper

--- lib/polymer-rails.rb
+++ lib/polymer-rails.rb
@@ -1,4 +1,3 @@
 require "polymer-rails/version"
 require "polymer-rails/helpers/assets_tag_helper"
-require "polymer-rails/helpers/assets_url_helper"
 require "polymer-rails/engine"
--- lib/polymer-rails/helpers/assets_tag_helper.rb
+++ lib/polymer-rails/helpers/assets_tag_helper.rb
@@ -5,7 +5,7 @@ module ActionView
         options = sources.extract_options!
         path_options = options.extract!('protocol').symbolize_keys
         sources.uniq.map do |source|
-          tag_options = { rel: "import", href: html_path(source, path_options) }
+          tag_options = { rel: "import", href: path_to_asset(source, {type: :html, extname: '.html'}.merge!(path_options)) }
           tag(:link, tag_options.merge!(options).stringify_keys)
         end.join("\n").html_safe
       end
