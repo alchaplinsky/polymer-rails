232205a33ef9802038a030fbd2305a9343cc457e
Alex Chaplinsky <alchapone@yandex.ru>
2014-07-01 21:35:08 +0300

    Added initializer to include helper

--- lib/polymer-rails.rb
+++ lib/polymer-rails.rb
@@ -1,3 +1,3 @@
 require "polymer-rails/version"
 require "polymer-rails/helpers/asset_tag_helper"
-require "polymer-rails/engine"
+require "polymer-rails/engine" if defined?(::Rails)
--- lib/polymer-rails/engine.rb
+++ lib/polymer-rails/engine.rb
@@ -2,9 +2,13 @@ module Polymer
   module Rails
     class Engine < ::Rails::Engine
       config.assets.precompile += %w( polymer/polymer.js )
-      ActionView::Helpers::AssetTagHelper.module_eval do
-        include AssetTagHelper
+
+      initializer :polymer_html_import do
+        helpers = %q{ include AssetTagHelper }
+        ::ActionView::Helpers::AssetTagHelper.module_eval(helpers)
+        ::Rails.application.assets.context_class.class_eval(helpers)
       end
+
     end
   end
 end
