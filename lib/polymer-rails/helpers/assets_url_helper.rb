b86c3c4c83d65dfccc57d192ea2ab07673e6c4ba
Alex Chaplinsky <alchapone@yandex.ru>
2014-07-01 20:58:54 +0300

    Removed patching ASSET_EXTENSIONS constant

--- lib/polymer-rails/helpers/assets_url_helper.rb
+++ lib/polymer-rails/helpers/assets_url_helper.rb
@@ -1,12 +1,11 @@
 module ActionView
   module Helpers
     module AssetUrlHelper
-      
-      ASSET_EXTENSIONS.merge!(html: '.html' )
+
       ASSET_PUBLIC_DIRECTORIES.merge!(html: '/components' )
 
       def html_path(source, options = {})
-        path_to_asset(source, {type: :html}.merge!(options))
+        path_to_asset(source, {type: :html, extname: '.html'}.merge!(options))
       end
 
     end
