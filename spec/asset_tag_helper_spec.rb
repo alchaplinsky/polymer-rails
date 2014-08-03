704334cea56ec5c34d681e33ea1bf96f564cb117
Alex Chaplinsky <alchapone@yandex.ru>
2014-08-03 13:51:10 +0400

    Added helper test

---
+++ spec/asset_tag_helper_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+require 'action_view'
+require 'polymer-rails/helpers/asset_tag_helper'
+
+class AssetTagHelperTest
+end
+
+describe Polymer::Rails::AssetTagHelper do
+
+  before do
+    @dummy_class = AssetTagHelperTest.new
+    @dummy_class.extend(ActionView::Helpers::TagHelper)
+    @dummy_class.extend(ActionView::Helpers::AssetTagHelper)
+    @dummy_class.extend(Polymer::Rails::AssetTagHelper)
+  end
+
+  it 'generates html import tag' do
+    expect(@dummy_class.html_import_tag('application')).to eq("<link href=\"/application.html\" rel=\"import\" />")
+  end
+
+end
