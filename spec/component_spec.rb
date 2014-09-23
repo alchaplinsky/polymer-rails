7c51a0e3dac06dc8e6b723ef822996c6e8fc28ec
still <still@gettyimages.com>
2014-09-23 12:09:29 -0600

    fixed test

--- spec/component_spec.rb
+++ spec/component_spec.rb
@@ -5,6 +5,7 @@ require 'polymer-rails/component'
 describe Polymer::Rails::Component do
   let(:data){ '
       <link rel="stylesheet" href="1.css" />
+      <link rel="stylesheet" href="2.css" shim-shadowdom />
       <script type="text/javascript" src="1.js"></script>
       <script type="text/javascript" src="2.js"></script>
       <link rel="import" href="1.html" />
@@ -12,16 +13,33 @@ describe Polymer::Rails::Component do
   }
 
   context '#replace_node' do
-    subject do
-      doc = described_class.new(data)
-      node = doc.stylesheets[0]
-      doc.replace_node node, "style", "body{color: red;}"
-      doc
+    context 'has shim-shadowdom attribute' do
+      subject do
+        doc = described_class.new data
+        node = doc.stylesheets[1]
+        doc.replace_node node, "style", "body { margin: 0px; }"
+        doc
+      end
+      
+      it 'replaces the stylesheet link with the inline styles and has the shim-shadowdom attr' do
+        expect(subject.stringify).to include("<style shim-shadowdom>\nbody { margin: 0px; }\n</style>")
+      end
     end
-
-    it 'replaces stylesheet link with inline styles' do
-      expect(subject.stringify).to include("<style>\nbody{color: red;}\n</style>")
-      expect(subject.stringify).not_to include('<link rel="stylesheet"')
+    
+    context 'does not have the shim-shadowdom attribute' do
+      subject do
+        doc = described_class.new(data)
+        nodes = doc.stylesheets
+        nodes.each do |node|
+          doc.replace_node node, "style", "body{color: red;}"
+        end
+        doc
+      end
+  
+      it 'replaces stylesheet link with inline styles' do
+        expect(subject.stringify).to include("<style>\nbody{color: red;}\n</style>")
+        expect(subject.stringify).not_to include('<link rel="stylesheet"')
+      end
     end
   end
 
@@ -59,7 +77,7 @@ describe Polymer::Rails::Component do
     subject{ described_class.new(data).stylesheets }
 
     it 'returns array of nodes' do
-      expect(subject.size).to eq 1
+      expect(subject.size).to eq 2
       expect(subject[0]).to be_kind_of(Hpricot::Node)
     end
 
