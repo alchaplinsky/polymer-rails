10d7433052f9d3dd0fe86f6f1f17d0d4e3addf44
steve <till.steve@gmail.com>
2014-11-01 22:32:17 -0600

    seperated the css and js from the main template into their own scss and coffee flies

--- lib/generators/polymer/component/component_generator.rb
+++ lib/generators/polymer/component/component_generator.rb
@@ -4,11 +4,13 @@ module Polymer
       source_root File.expand_path('../templates', __FILE__)
 
       def create_component_dir
-        empty_directory "app/assets/components/#{file_name}"
+        empty_directory "app/assets/components/#{name}"
       end
 
       def copy_component_template
-        template "component.html.erb", "app/assets/components/#{file_name}/#{file_name}.html"
+        template "component.html.erb", "app/assets/components/#{name}/#{name}.html"
+        template "component.js.coffee.erb", "app/assets/components/#{name}/#{name}.js.coffee"
+        template "component.css.scss.erb", "app/assets/components/#{name}/#{name}.css.scss"
       end
 
     end
