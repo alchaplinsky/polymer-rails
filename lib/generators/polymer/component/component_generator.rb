a55351bcd14da78121f2715488cdcbe099b9b67e
Alex Chaplinsky <alchaplinsky@gmail.com>
2014-11-28 03:57:12 +0700

    Small generator changes

--- lib/generators/polymer/component/component_generator.rb
+++ lib/generators/polymer/component/component_generator.rb
@@ -4,15 +4,20 @@ module Polymer
       source_root File.expand_path('../templates', __FILE__)
 
       def create_component_dir
-        empty_directory "app/assets/components/#{name}"
+        empty_directory "app/assets/components/#{component_name}"
       end
 
       def copy_component_template
-        template "component.html.erb", "app/assets/components/#{name}/#{name}.html"
-        template "component.js.coffee.erb", "app/assets/components/#{name}/#{name}.js.coffee"
-        template "component.css.scss.erb", "app/assets/components/#{name}/#{name}.css.scss"
+        template "component.html.erb", "app/assets/components/#{component_name}/#{component_name}.html"
+        template "component.js.coffee.erb", "app/assets/components/#{component_name}/#{component_name}.js.coffee"
+        template "component.css.scss.erb", "app/assets/components/#{component_name}/#{component_name}.css.scss"
       end
 
+      private
+
+      def component_name
+        name.gsub('_', '-').downcase
+      end
     end
   end
 end
